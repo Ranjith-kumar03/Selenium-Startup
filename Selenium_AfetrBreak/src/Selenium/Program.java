0e5f517a0ad22d2da629990f1b3d3504b9f35402
ranji <ranji@DESKTOP-LUMCGL2>
2018-09-05 18:11:25 +0530

    First push

---
+++ Selenium_AfetrBreak/src/Selenium/Program.java
@@ -0,0 +1,33 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Program {
+	
+	
+	@Test
+	public void chrometest() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ranji\\OneDrive\\Desktop\\Chrome\\chromedriver_win32\\chromedriver.exe");
+				
+		
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	WebElement element =driver.findElement(By.name("email"));
+	element.sendKeys("ranjith.kumar03@gnmail.com");
+	element =driver.findElement(By.name("pass"));
+	element.sendKeys("adiranjith1980");
+	element =driver.findElement(By.id("loginbutton"));
+	element.click();
+	
+Thread.sleep(10000);
+
+driver.quit();
+	}
+
+}
